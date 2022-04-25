fea377b36173827a7b2590c211cf474eb45c9288
jungu0416 <junguYY0809@gmail.com>
2022-04-25 10:20:27 +0900

    ㅈㅅ

--- src/main/java/com/example/gw/services/UserService.java
+++ src/main/java/com/example/gw/services/UserService.java
@@ -1,7 +1,5 @@
 package com.example.gw.services;
 
-import com.example.gw.mappers.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
