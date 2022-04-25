43fcd37a953ddf5ba48bc04098bf538946c50c0d
jungu0416 <junguYY0809@gmail.com>
2022-04-25 11:23:30 +0900

    jasypt

--- src/main/java/com/example/gw/GwApplication.java
+++ src/main/java/com/example/gw/GwApplication.java
@@ -10,4 +10,5 @@ public class GwApplication {
         SpringApplication.run(GwApplication.class, args);
     }
 
+
 }
--- src/test/java/com/example/gw/GwApplicationTests.java
+++ src/test/java/com/example/gw/GwApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.gw;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,26 @@ class GwApplicationTests {
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void jasypt() {
+        String url = "jdbc:mariadb://52.78.185.210:3306/egline";
+        String username = "egline";
+        String password = "0000";
+
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "my_jasypt_key";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
+
 
 }
