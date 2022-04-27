086b9fd1a171ec510eadea25dc1401ebc3d122d1
ihatelua <ponyosushi@gmail.com>
2022-04-27 14:35:05 +0900

    템플릿 정리 및 로딩바 수정

--- src/main/java/com/example/gw/main/controllers/PortletController.java
+++ src/main/java/com/example/gw/main/controllers/PortletController.java
@@ -1,13 +1,21 @@
 package com.example.gw.main.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PortletController {
 
     @GetMapping("/portletMain")
-    public String portletPage() {
-        return "portletMain";
+    public String portletPage(Model model) {
+        model.addAttribute("headerYn", "Y");
+        model.addAttribute("headerPath", "ui/pltl/portletHeader");
+        model.addAttribute("headerId", "portletHeader");
+
+        model.addAttribute("mainPath", "ui/pltl/portletMain");
+        model.addAttribute("mainId", "portletMain");
+
+        return "CommonTemplate";
     }
 }
