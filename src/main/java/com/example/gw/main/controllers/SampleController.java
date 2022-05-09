e9f9a374ceec7257926b97d5dd7674002ebe8274
ponyosushi <ponyosushi@gmail.com>
2022-05-09 17:21:32 +0900

    공통컨트롤러 구현

--- src/main/java/com/example/gw/main/controllers/SampleController.java
+++ src/main/java/com/example/gw/main/controllers/SampleController.java
@@ -11,22 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/sample")
 public class SampleController {
     @Autowired
     private ICommonService commonService;
 
-    @GetMapping("/sample.do")
-    public String portletPage(Model model) throws Exception{
-        // 샘플 header
-        model.addAttribute("headerYn", "Y");    // 샘플 header 적용여부
-        model.addAttribute("headerPath", "sample/sample-header"); // 샘플 header의 경로
-        model.addAttribute("headerId", "sample-header");          // 샘플 headerId
-
-        // 샘플 main
-        model.addAttribute("mainPath", "sample/sample"); // 샘플 main의 경로
-        model.addAttribute("mainId", "sample-body");          // 샘플 mainId
-
-        return "CommonTemplate"; // 무조건 CommonTemplate 로 경로지정(공통템플릿임)
-    }
+  /*
+  * ajax만 선언하면됨
+  * */
 }
