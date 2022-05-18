5ec152d15fd0f9460dc39b2a1782f985f8a6d2cd
ponyosushi <ponyosushi@gmail.com>
2022-05-18 17:52:26 +0900

    메뉴관리 이벤트 커밋

--- src/main/resources/static/js/manager/createMenu.js
+++ src/main/resources/static/js/manager/createMenu.js
@@ -1,19 +1,25 @@
-// 모듈 사용을 위한 jquery 문
-$(document).ready(function() {
-    // $('.dd').nestable();
-    //
-    // $('.dd').on('change', function () {
-    //     var $this = $(this);
-    //     var serializedData = window.JSON.stringify($($this).nestable('serialize'));
-    //     $this.parents('div.body').find('textarea').val(serializedData);
-    // });
-});
+// 전역변수
+const commonMenu = new CommonMenu();
+let reflectMenuList = [];
 
 window.onload = () =>{
-    // 메뉴 경로 세팅
-    let commonMenu = new CommonMenu();
-    commonMenu.setContentHeader("manager/createMenu");
+    initPage();
+}
 
-    // 메뉴포틀릿 세팅
-    CommonModule.nestable('.dd', {"maxDepth":2});
+const initPage = () => {
+    commonMenu.setContentHeader("manager/createMenu");      // 메뉴 경로 세팅
+    CommonModule.nestable('.dd', {"maxDepth":2});    // 메뉴포틀릿 세팅
+
+    setEvent();
+}
+
+const setEvent = () =>{
+    // 반영될 메뉴리스트 이벤트
+    const reflectMenu = document.getElementById('reflectMenu');
+    const reflectTextarea = document.getElementById("reflectTextarea");
+    reflectMenu.children[0].addEventListener('mouseup', (event) => {
+        reflectMenuList = CommonModule.nestableValues(reflectMenu);
+        reflectTextarea.innerText = reflectMenuList;
+    });
+
 }
