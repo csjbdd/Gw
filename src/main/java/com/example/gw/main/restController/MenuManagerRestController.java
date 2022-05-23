8dcbb9dffcd91ba5f4b433c34d8a35f0d9d4cb93
ponyosushi <ponyosushi@gmail.com>
2022-05-23 14:02:53 +0900

    메뉴관리 데이터정제 및 세팅

--- src/main/java/com/example/gw/main/restController/MenuManagerRestController.java
+++ src/main/java/com/example/gw/main/restController/MenuManagerRestController.java
@@ -1,5 +1,6 @@
 package com.example.gw.main.restController;
 
+import com.example.gw.common.dao.ICommonDAO;
 import com.example.gw.common.enums.StatusEnum;
 import com.example.gw.common.service.ICommonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,22 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.gw.common.dto.Massage;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/manager/*")
 public class MenuManagerRestController {
     @Autowired
-    ICommonService commonService;
+    private ICommonService commonService;
 
     @PostMapping("/findMenuList")
-    public ResponseEntity<Object> pwEmailSend() {
+    public ResponseEntity<Object> pwEmailSend() throws Exception {
         Massage message = new Massage();
         HttpHeaders headers= new HttpHeaders();
         Map<String, Object> map = new HashMap<>();
+        List<Object> menuList = null;
 
-        map.put("data", "ㅇㄱㄹㅇ");
-        
+        // 메뉴리스트 가져오기
+        menuList = commonService.selectList("MenuMapper.selectManagerMenuList");
+        int menuSize = menuList.size();
+
+        map.put("menuList", menuList);
         message.setStatus(StatusEnum.OK);
         message.setMessage("성공 코드");
         message.setData(map);
